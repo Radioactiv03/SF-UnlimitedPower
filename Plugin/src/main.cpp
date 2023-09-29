ac8be92f87ad4b76382bd9d6de332edff10855f5
Radioactiv03 <52461475+Radioactiv03@users.noreply.github.com>
2023-09-29 01:20:26 +0100

    post update patch

--- Plugin/src/main.cpp
+++ Plugin/src/main.cpp
@@ -12,6 +12,9 @@ namespace
 		switch (a_msg->type) {
 		case SFSE::MessagingInterface::kPostLoad:
 			{
+				auto& trampoline = SFSE::GetTrampoline();
+				//Need to find addr again
+				//trampoline.writecall
 			}
 			break;
 		default:
@@ -39,8 +42,8 @@ DLLEXPORT bool SFSEAPI SFSEPlugin_Load(const SFSE::LoadInterface* a_sfse)
 	// this allocates 1024 bytes for development builds, you can
 	// adjust the value accordingly with the log result for release builds
 	//SFSE::AllocTrampoline(1 << 10);
-	auto                            sig = "C5????????488B????C6????????488B??????C5??????????4883????5FC3????4889";  //,0x0,0x248,0x40,0xC0
-	REL::Relocation<std::uintptr_t> target{ REL::ID(779743), 0x80 };
+	auto sig = "C5????????488B????C6????????488B??????C5??????????4883????5FC3????4889";  //,0x0,0x248,0x40,0xC0
+	//REL::Relocation<std::uintptr_t> target{ REL::ID(779743), 0x80 };
 	SFSE::GetMessagingInterface()->RegisterListener(MessageCallback);
 	return true;
 }
