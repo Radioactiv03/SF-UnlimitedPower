8b6b3ce55da72971d0c424bd7a3821a48868b383
Radioactiv03 <52461475+Radioactiv03@users.noreply.github.com>
2023-10-05 01:32:11 +0100

    Update main.cpp

--- Plugin/src/main.cpp
+++ Plugin/src/main.cpp
@@ -7,14 +7,47 @@
 
 namespace
 {
+	RE::ConsoleLog* console;
+
+	/*
+	static DWORD ThreadProc_OnDelayLoad(void* unused)
+	{
+		(void)unused;
+		while (!RE::TESForm::LookupByID(0x14)) // Wait until forms are loaded, then run startup commands
+		{
+			Sleep(1000);
+		}
+		console = RE::ConsoleLog::GetSingleton();
+
+		return 0;
+	}
+	*/
+	void Install()
+	{
+		
+		void* addr = dku::Hook::Assembly::search_pattern<
+		"C5 ?? ?? ?? ?? "
+		"48 8B ?? ?? "
+		"C6 ?? ?? ?? ?? "
+		"48 8B ?? ?? ?? "
+		"C5 ?? ?? ?? ?? ?? "
+		"48 83 ?? ?? "
+		"5F C3 ?? ?? "
+		"48 89">();
+		
+		int t = *(int*)addr;
+		t = 0x90;
+		
+	}
+
 	void MessageCallback(SFSE::MessagingInterface::Message* a_msg) noexcept
 	{
 		switch (a_msg->type) {
 		case SFSE::MessagingInterface::kPostLoad:
 			{
-				auto& trampoline = SFSE::GetTrampoline();
-				//Need to find addr again
-				//trampoline.writecall
+				
+				//CreateThread(NULL, 4096, ThreadProc_OnDelayLoad, NULL, 0, NULL);
+				Install();
 			}
 			break;
 		default:
@@ -42,8 +75,9 @@ DLLEXPORT bool SFSEAPI SFSEPlugin_Load(const SFSE::LoadInterface* a_sfse)
 	// this allocates 1024 bytes for development builds, you can
 	// adjust the value accordingly with the log result for release builds
 	//SFSE::AllocTrampoline(1 << 10);
-	auto sig = "C5????????488B????C6????????488B??????C5??????????4883????5FC3????4889";  //,0x0,0x248,0x40,0xC0
-	//REL::Relocation<std::uintptr_t> target{ REL::ID(779743), 0x80 };
+	//auto sig = "C5????????488B????C6????????488B??????C5??????????4883????5FC3????4889"; //,0x0,0x248,0x40,0xC0
+
+
 	SFSE::GetMessagingInterface()->RegisterListener(MessageCallback);
 	return true;
 }
